d5067e2ade517561358116934236c5ee0f0c2061
David Castillo <davecstillo@gmail.com>
2018-09-19 11:54:40 -0600

    Arreglo del error de hoy
    El error estaba en que estaba escribiendo color con la primera minusculas, y la libreria se llama "Color" con la primera en mayusculas

--- core/src/com/progra/nuclearwar/Hud.java
+++ core/src/com/progra/nuclearwar/Hud.java
@@ -35,13 +35,21 @@ public class Hud {
         playerhud.top();
         playerhud.setFillParent(true);
 
+        //Cambio desde la ultima clase miercoles19/sept/2018
 
-//        timertxt = new Label(String.format("%03d", worldtimer), new Label.LabelStyle(new BitmapFont(), );
-//        scorelabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), color.White);
-//        time = new Label("Time", new Label.LabelStyle(new BitmapFont, color.White);
-//        worldnmb = new Label("1-1", new Label.LabelStyle(new BitmapFont, color.White);
-//        world = new Label("World", new Label.LabelStyle(new BitmapFont, color.White);
-//        name = new Label("Player", new Label.LabelStyle(new BitmapFont, color.White);
+        /*
+        * se hizo el importe de la libreria de badlogic para los colores...   "com.badlogic.gdx.graphics.Color"
+        * y con esto se tiene el color, el error que yo tuve, fue que lo estaba escribiendo con minuscula, Por eso es importante
+        * tener en cuenta que los nombres de las clases se escriben con mayusculas y los nombres de las variables se escriben con minusculas
+        *
+        */
+
+        timertxt = new Label(String.format("%03d", worldtimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scorelabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        time = new Label("Time", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        worldnmb = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        world = new Label("World", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        name = new Label("Player", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
 
 
