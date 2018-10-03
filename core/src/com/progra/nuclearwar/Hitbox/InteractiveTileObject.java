801ec77059e680aad8aa3a3cdd95f6d1fe8e1787
David Castillo <davecstillo@gmail.com>
2018-10-03 09:58:52 -0600

    InteractiveTileObject

--- core/src/com/progra/nuclearwar/Hitbox/InteractiveTileObject.java
+++ core/src/com/progra/nuclearwar/Hitbox/InteractiveTileObject.java
@@ -1,5 +1,60 @@
 package com.progra.nuclearwar.Hitbox;
 
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Filter;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.progra.nuclearwar.NuclearWarGame;
+import com.progra.nuclearwar.Screens.PlayScreen;
+
 public abstract class InteractiveTileObject {
-    
+    protected World world;
+    protected TiledMap map;
+    protected TiledMapTile tile;
+    protected Rectangle bounds;
+    protected Body body;
+    protected Fixture fixture;
+    public boolean DoorOpen;
+
+    public InteractiveTileObject(PlayScreen screen, Rectangle bounds) {
+        this.world = screen.getWorld();
+        this.map = screen.getMap();
+        this.bounds = bounds;
+        BodyDef bdef = new BodyDef();
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        DoorOpen = false;
+
+        bdef.type = BodyDef.BodyType.StaticBody;
+        bdef.position.set((bounds.getX() + bounds.getWidth() / 2)  / NuclearWarGame.PPM, (bounds.getY() + bounds.getHeight() / 2)  / NuclearWarGame.PPM);
+        body = world.createBody(bdef);
+
+        shape.setAsBox((bounds.getWidth()/2) / NuclearWarGame.PPM , (bounds.getHeight()/2)  / NuclearWarGame.PPM);
+        fdef.shape = shape;
+        fixture = body.createFixture(fdef);
+
+    }
+    public  abstract void onHeadHit();
+    public abstract void onFeetHit();
+
+    public void setCategoryFilter(short filterBit){
+        Filter filter = new Filter();
+        filter.categoryBits = filterBit;
+        fixture.setFilterData(filter);
+    }
+
+    public boolean isDoorOpen(){
+        return DoorOpen;
+    }
+    public void setDoorOpen(boolean isOpen){
+        this.DoorOpen = isOpen;
+    }
+
 }
