ad54c34f51e0b6358e77b7bffcd477ea3ea44678
GabrielGallegos <gaboven00@Outlook.com>
2018-11-05 09:47:13 -0600

    Stronghold

---
+++ android/assets/mapas/Stronghold_FloorTorch.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Stronghold_FloorTorch" tilewidth="16" tileheight="16" tilecount="8" columns="4">
+ <image source="../Multi_Platformer_Tileset_v2/Stronghold/Details/Animated/Stronghold_FloorTorch.png" width="64" height="32"/>
+ <tile id="2">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
