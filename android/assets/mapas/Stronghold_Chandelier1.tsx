d02d7defd3dca07194811f0295f2bf1cd79871e2
GabrielGallegos <gaboven00@Outlook.com>
2018-11-09 09:11:19 -0600

    Cambios a Castillo

---
+++ android/assets/mapas/Stronghold_Chandelier1.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Stronghold_Chandelier1" tilewidth="16" tileheight="16" tilecount="24" columns="12">
+ <image source="../Multi_Platformer_Tileset_v2/Stronghold/Details/Animated/Stronghold_Chandelier1.png" width="192" height="32"/>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="21" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="22" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="23" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
